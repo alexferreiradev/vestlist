4f736d8598d9784eddaa7411b8dfe0f84c28bd3a
Alex R. Ferreira <arf92@live.com>
2017-03-19 15:17:29 -0300

    remocao de metodos nao utilizados

--- projeto_android/Vestlist/app/src/main/java/com/alex/vestlist/ui/view/BaseListViewActivity.java
+++ projeto_android/Vestlist/app/src/main/java/com/alex/vestlist/ui/view/BaseListViewActivity.java
@@ -1,24 +1,18 @@
 package com.alex.vestlist.ui.view;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.alex.vestlist.model.BaseModel;
 import com.alex.vestlist.ui.presenter.BaseListContract;
 import com.alex.vestlist.ui.presenter.BaseListPresenter;
 
-import java.util.List;
-
 import static com.alex.vestlist.R.id.emptyView;
 import static com.alex.vestlist.R.id.listView;
-import static com.alex.vestlist.R.id.progressBar;
 
 /**
  * Created by Alex on 17/03/2017.
@@ -27,20 +21,11 @@ import static com.alex.vestlist.R.id.progressBar;
 abstract class BaseListViewActivity<ModelType extends BaseModel,
         Presenter extends BaseListPresenter,
         ViewType extends BaseListContract.View>
-        extends BaseActivity
+        extends BaseActivity<ModelType, ViewType, Presenter>
         implements AbsListView.OnScrollListener, BaseListContract.View<ModelType> {
 
-    protected int mLoadItemsLimit;
-    protected int mOffset;
-    protected Presenter mPresenter;
     protected ListView mListView;
     protected TextView mEmptyView;
-    protected ProgressBar mProgressBar;
-
-    /**
-     * valor inicial para total a ser carregado
-     */
-    protected static final int LIMIT_INITIAL = 20;
 
     @Override
     public void toggleProgressBar() {
@@ -64,52 +49,37 @@ abstract class BaseListViewActivity<ModelType extends BaseModel,
     }
 
     @Override
-    public void resetPagingCounters() {
-        mLoadItemsLimit = LIMIT_INITIAL;
-        mOffset = 0;
-        mListView.setAdapter(null);
-    }
-
-    @Override
-    public void setOffsetValue(int newValue) {
-        mOffset = newValue;
-    }
-
-    @Override
-    public void setLoadItemsLimitValue(int newLimit) {
-        mLoadItemsLimit = newLimit;
-    }
-
-    @Override
-    public void showErrorMsg(String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void showSuccessMsg(String msg) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
     protected void onStart() {
         super.onStart();
         if (mPresenter != null)
-            mPresenter.reloadList();
+            mPresenter.reCreateAdapter();
     }
 
     @Override
-    public void initializeWidgets(Bundle savedInstanceState){
+    public void initializeWidgets(Bundle savedInstanceState) {
+        super.initializeWidgets(savedInstanceState);
         mListView = (ListView) findViewById(listView);
         mEmptyView = (TextView) findViewById(emptyView);
         mListView.setEmptyView(mEmptyView);
         mListView.setOnScrollListener(this);
         mListView.setOnItemClickListener(this);
-        mProgressBar = (ProgressBar) findViewById(progressBar);
     }
 
     @Override
     public void onScrollStateChanged(AbsListView view, int scrollState) {
 
+    }
+
+    /**
+     * @param view
+     * @param firstVisibleItem - posicao do item da primeira celula
+     * @param visibleItemCount - total de células visiveis
+     * @param totalItemCount   - total no adapter
+     */
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        if (mPresenter != null && totalItemCount > 0)
+            mPresenter.loadMoreData(firstVisibleItem, visibleItemCount, totalItemCount);
     }
 
     @Override
@@ -120,40 +90,4 @@ abstract class BaseListViewActivity<ModelType extends BaseModel,
         }
     }
 
-    /**
-     *
-     * @param view
-     * @param firstVisibleItem - posicao do item da primeira celula
-     * @param visibleItemCount - total de células visiveis
-     * @param totalItemCount - total no adapter
-     */
-    @Override
-    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-        if (mPresenter != null && mOffset > 0)
-            mPresenter.loadMoreData(mOffset, mLoadItemsLimit, firstVisibleItem, visibleItemCount, totalItemCount);
-    }
-
-    @Override
-    public void startLoadThread(int offset) {
-        String[] params = {String.valueOf(offset)};
-        new LoadSubjects().execute(params);
-    }
-
-    public class LoadSubjects extends AsyncTask<String, Integer, List> {
-
-        @Override
-        protected List doInBackground(String... params) {
-            String offsetString = params[0];
-            if (mPresenter == null)
-                return null;
-
-            return mPresenter.loadDataFromSource(Integer.parseInt(offsetString), mLoadItemsLimit);
-        }
-
-        @Override
-        protected void onPostExecute(List list) {
-            if (mPresenter != null)
-                mPresenter.populateList(list, mOffset);
-        }
-    }
 }
