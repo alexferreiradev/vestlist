6a2ef8003ca2abc7458e93ab4db68ff9b1f9f4c2
Alex R. Ferreira <arf92@live.com>
2017-03-19 00:30:23 -0300

    implement show list methods.

--- projeto_android/Vestlist/app/src/main/java/com/alex/vestlist/ui/presenter/DoubtPresenter.java
+++ projeto_android/Vestlist/app/src/main/java/com/alex/vestlist/ui/presenter/DoubtPresenter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.os.Bundle;
 
 import com.alex.vestlist.model.Doubt;
+import com.alex.vestlist.model.ExerciseList;
+import com.alex.vestlist.ui.util.ViewUtil;
 
 import java.util.List;
 
@@ -19,7 +21,7 @@ public class DoubtPresenter extends BaseListPresenter<DoubtPresenter.View, Doubt
 
     @Override
     public List<Doubt> loadDataFromSource(int offset, int loadItemsLimit) {
-        return null;
+        return mSource.loadDoubts(mView.getExerciseList(), offset, loadItemsLimit);
     }
 
     @Override
@@ -29,35 +31,37 @@ public class DoubtPresenter extends BaseListPresenter<DoubtPresenter.View, Doubt
 
     @Override
     protected void showSuccessMsg() {
-
+        // TODO
     }
 
     @Override
     protected void showErrorMsg() {
-
+        // TODO
     }
 
     @Override
     public void populateFilteredList(String filterType) {
-
+        // TODO
     }
 
     @Override
     public void showAddOrEditView(Doubt data) {
-
+        // TODO
     }
 
     @Override
-    public int updateModelInSource(Doubt data) {
-        return 0;
+    public Long updateModelInSource(Doubt data) {
+        return mSource.updateDoubt(data);
     }
 
     @Override
-    public void openDataDetails(Doubt item) {
-
+    public void selectItemClicked(Doubt item) {
+        ViewUtil.showNotImplementedMsg(mContext);
     }
 
     public interface View extends BaseListContract.View<Doubt>{
+
+        public ExerciseList getExerciseList();
 
     }
 }
