8fd77dd5505ca1bf3df6fb29b14746bd4a2e66d9
Alex R. Ferreira <arf92@live.com>
2017-03-19 00:55:26 -0300

    rename para AddOrEditDoubtActivity

---
+++ projeto_android/Vestlist/app/src/main/java/com/alex/vestlist/ui/view/AddOrEditDoubt.java
@@ -0,0 +1,14 @@
+package com.alex.vestlist.ui.view;
+
+import android.os.Bundle;
+
+import com.alex.vestlist.R;
+
+public class AddOrEditDoubt extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_doubt);
+    }
+}
