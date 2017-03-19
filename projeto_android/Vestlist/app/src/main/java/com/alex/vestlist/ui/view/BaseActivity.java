24664a7bb55f7f66c05c41988c1e6ec70d96e7cd
Alex R. Ferreira <arf92@live.com>
2017-03-19 14:42:10 -0300

    add toogle progress e startthread.

--- projeto_android/Vestlist/app/src/main/java/com/alex/vestlist/ui/view/BaseActivity.java
+++ projeto_android/Vestlist/app/src/main/java/com/alex/vestlist/ui/view/BaseActivity.java
@@ -1,11 +1,56 @@
 package com.alex.vestlist.ui.view;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.alex.vestlist.model.BaseModel;
+import com.alex.vestlist.ui.presenter.BasePresenter;
 
 /**
  * Created by Alex on 16/03/2017.
  */
 
-public abstract class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity<ModelType extends BaseModel,
+        ViewType extends BasePresenter.View,
+        PresenterType extends BasePresenter>
+        extends AppCompatActivity
+        implements BasePresenter.View<ModelType>{
 
+    protected BasePresenter.TaskType mTaskType;
+    protected ModelType mData;
+    protected PresenterType mPresenter;
+    protected ProgressBar mProgressBar;
+
+    @Override
+    public void toggleProgressBar() {
+        if (mProgressBar.getVisibility() == View.VISIBLE)
+            mProgressBar.setVisibility(View.GONE);
+        else
+            mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void startBackgroundThread(ModelType data, BasePresenter.TaskType taskType) {
+        new BackgroundTask().execute();
+    }
+
+    private final class BackgroundTask extends AsyncTask<String, Integer, Object>{
+        @Override
+        protected Object doInBackground(String... params) {
+            if (mPresenter == null)
+                return null;
+
+            return mPresenter.taskFromSource(mData, mTaskType);
+        }
+
+        @Override
+        protected void onPostExecute(Object result) {
+            super.onPostExecute(result);
+            if (mPresenter != null){
+                mPresenter.analiseBackgroundThreadResult(result, mTaskType);
+            }
+        }
+    }
 }
