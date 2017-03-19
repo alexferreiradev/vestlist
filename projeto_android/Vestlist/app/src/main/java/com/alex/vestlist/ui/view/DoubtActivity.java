906413456f3223dc5f9cd453a2fdf8ed8dbf72e2
Alex R. Ferreira <arf92@live.com>
2017-03-19 00:32:41 -0300

    implement get para mList e recuperacao por intent

--- projeto_android/Vestlist/app/src/main/java/com/alex/vestlist/ui/view/DoubtActivity.java
+++ projeto_android/Vestlist/app/src/main/java/com/alex/vestlist/ui/view/DoubtActivity.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 
 import com.alex.vestlist.R;
 import com.alex.vestlist.model.Doubt;
+import com.alex.vestlist.model.ExerciseList;
+import com.alex.vestlist.ui.adapter.DoubtListViewAdapter;
 import com.alex.vestlist.ui.presenter.DoubtPresenter;
 
 import java.util.List;
 
 public class DoubtActivity extends BaseListViewActivity<Doubt, DoubtPresenter, DoubtPresenter.View> implements DoubtPresenter.View {
+
+    public static final String ARGUMENT_LIST_KEY = "list argument";
+    private DoubtListViewAdapter mAdapter;
+    private ExerciseList mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,24 +23,35 @@ public class DoubtActivity extends BaseListViewActivity<Doubt, DoubtPresenter, D
         mPresenter = new DoubtPresenter(this, this, savedInstanceState);
     }
 
+    @Override
+    public void initializeWidgets(Bundle savedInstanceState) {
+        mList = (ExerciseList) getIntent().getExtras().getSerializable(ARGUMENT_LIST_KEY);
+        super.initializeWidgets(savedInstanceState);
+    }
 
     @Override
     public void createListAdapter(List results) {
-
+        mAdapter = new DoubtListViewAdapter(this, results, R.layout.adapter_subject_listview);
+        mListView.setAdapter(mAdapter);
     }
 
     @Override
     public void showAddOrEditDataView() {
-
+        // TODO
     }
 
     @Override
     public void addAdapterData(List<Doubt> result) {
-
+        mAdapter.addAll(result);
     }
 
     @Override
     public void startSaveOrEditThread(Doubt data) {
+        // TODO
+    }
 
+    @Override
+    public ExerciseList getExerciseList() {
+        return mList;
     }
 }
